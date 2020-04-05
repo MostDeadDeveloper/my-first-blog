41dc54f74f60eeddbece8ed4e79de91fd3523a9c
Epicguy12431 <vinascarljohn.125@gmail.com>
2020-04-05 17:00:46 +0800

    My Django Girls app, first commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/robertwaltson/Documents/Projects/django_girls_tutorial/Project1/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
